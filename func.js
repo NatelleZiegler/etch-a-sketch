3227ad505d5bcc0f985efeebd054de7c2e4e5468
Natelle Ziegler <n.ziegler@ymail.com>
2018-01-04 05:53:28 -0500

    Finished project

--- func.js
+++ func.js
@@ -1,12 +1,21 @@
 const wrapper = document.querySelector('.wrapper');
-//Create a two-dimensional array
 var boxArr = [];
-for(var i = 0; i < 16 * 16; i++) {
-	boxArr[i] = document.createElement('div');
-	boxArr[i].classList.add('box');
-	boxArr[i].addEventListener("mouseover", mouseOver);
-	wrapper.appendChild(boxArr[i]);
+
+//Create a two-dimensional array
+function createGrid(size) {
+	while (wrapper.firstChild) {
+		wrapper.removeChild(wrapper.firstChild);
+	}
+	wrapper.style["grid-template-columns"] = `repeat(${size}, 1fr)`;
+	for(var i = 0; i < size * size; i++) {
+		boxArr[i] = document.createElement('div');
+		boxArr[i].classList.add('box');
+		boxArr[i].addEventListener("mouseover", mouseOver);
+		wrapper.appendChild(boxArr[i]);
+	}
 }
+
+createGrid(16);
 
 function mouseOver() {
     this.style.backgroundColor = "red";
@@ -16,7 +25,11 @@ const resetButton = document.getElementById('reset');
 resetButton.addEventListener("click", resetGrid);
 
 function resetGrid() {
+	//Clear the grid
 	for(var i = 0; i < boxArr.length; i++) {
 		boxArr[i].style.backgroundColor = "white";
 	}
+	//Prompt the user for the size of the grid
+	var size = prompt("How many squares per side for new grid?", "16");
+	createGrid(size);
 }
