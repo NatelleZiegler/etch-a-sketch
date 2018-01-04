c8dff8eb8a85549d3765a734ae4d55b35dc39d26
Natelle Ziegler <n.ziegler@ymail.com>
2018-01-04 04:21:34 -0500

    Created grid with functionality

---
+++ func.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector('.wrapper');
+//Create a two-dimensional array
+var boxArr = [];
+for(var i = 0; i < 16 * 16; i++) {
+	boxArr[i] = document.createElement('div');
+	boxArr[i].classList.add('box');
+	boxArr[i].addEventListener("mouseover", mouseOver);
+	wrapper.appendChild(boxArr[i]);
+}
+
+function mouseOver() {
+    this.style.backgroundColor = "red";
+}
+
+const resetButton = document.getElementById('reset');
